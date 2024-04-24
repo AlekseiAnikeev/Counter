c7ca0146e93b5f7817ecdc1e86a2355f58ac0d03
Aleksey Anikeev <90957150+AlekseiAnikeev@users.noreply.github.com>
2024-04-24 22:27:44 +0300

    Init commit

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private float _delay = 0.5f;
+
+    private bool _isButtonPressed = false;
+    private float _triggerTime;
+    private int _counter = 0;
+
+    private void Start()
+    {
+        _triggerTime = _delay;
+        _text.text = "0";
+
+        StartCoroutine(TestCoroutine());
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+            _isButtonPressed = !_isButtonPressed;
+    }
+
+    IEnumerator TestCoroutine()
+    {
+        while (enabled)
+        {
+            yield return new WaitUntil(() => _isButtonPressed);
+
+            if (Time.time >= _triggerTime)
+            {
+                _text.text = _counter++.ToString();
+                _triggerTime = Time.time + _delay;
+            }
+        }
+    }
+}
